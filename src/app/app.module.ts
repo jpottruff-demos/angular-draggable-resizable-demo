6ddfb495d339d91e3eddf01d4c2d9443287ae381
Jeff Pottruff <jeffpottruff@gmail.com>
2022-04-21 17:52:12 -0400

    adds cdkDrag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop'; 
 
 import { AppComponent } from './app.component';
 import { ResizableComponent } from './resizable/resizable.component';
 import { ItemComponent } from './item/item.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,8 @@ import { ItemComponent } from './item/item.component';
     ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
